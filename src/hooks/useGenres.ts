9ea345cb7f1a168d373b722e97f74bbd02ad2f51
Matthias Heilos <mheilos@fintechinnovations>
2024-02-07 19:24:54 -0800

    ship genres with app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
-import useData from "./useData";
+// import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
     id: number;
@@ -6,6 +7,7 @@ export interface Genre {
     image_background: string;
   }
 
-const useGenre = () => useData<Genre>('/genres')
+//const useGenre = () => useData<Genre>('/genres')
+const useGenre = () => ({data: genres, isLoading: false, error: null})
 
 export default useGenre
